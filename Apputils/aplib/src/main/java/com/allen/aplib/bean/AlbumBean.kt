e523b52cc164fb92b559363872a3332339a15fdf
lwl <215446422@qq.com>
2020-02-19 10:45:44 +0800

    1

---
+++ Apputils/aplib/src/main/java/com/allen/aplib/bean/AlbumBean.kt
@@ -0,0 +1,16 @@
+package com.allen.apputils.bean
+
+import java.io.Serializable
+
+/**
+ * @author Allen
+ * @date 2019/9/25 16:37
+ * @describe
+ */
+class AlbumBean : Serializable {
+    var filename: String? = null
+    var path: String? = null
+    var size: Long = 0
+    var type: Int = 0 //0  视频  1  图片
+    var isSelect: Boolean = false //是否选中，用于删除
+}
---
+++ Apputils/aplib/src/main/java/com/allen/aplib/bean/VideoInfo.kt
@@ -0,0 +1,18 @@
+package com.allen.apputils.bean
+
+/**
+ * @author Allen
+ * @date 2019/9/27 10:29
+ * @describe
+ */
+class VideoInfo {
+    var id: Int = 0
+    var name: String? = null
+    var path: String? = null
+    var dateModified: String? = null
+    var dateTaken: String? = null //创建时间
+    var duration: String? = null
+    var width: Int = 0
+    var height: Int = 0
+    var size: String? = null
+}
